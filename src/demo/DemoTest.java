5fc59a76b3cea03eb728b6cc6049f49b8223a874
User <User@DESKTOP-FUGUUKO.myfiosgateway.com>
2022-08-01 19:09:23 -0400

    Testing Commit

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,4 +5,14 @@ public class DemoTest {
 	public static void main (String [] args){
 		System.out.println("Test");
 	}
+	//pushing out code
+	public static void add(int a, int b) {
+		int result = a+b;
+		
+		System.out.println(result);
+	}
+	//for US 345
+	public static void divide(int a, int b) {
+		System.out.println(a/b);
+	}
 }
